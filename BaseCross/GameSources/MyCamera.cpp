a109de7587ac960ddda69b95562f1ee8ed94cac9
yuu2611 <zeal22300025@fsgcl.jp>
2023-10-17 09:51:31 +0900

    MyCamera.hとMyCamera.cppを追加

---
+++ BaseCross/GameSources/MyCamera.cpp
@@ -0,0 +1,12 @@
+/*!
+@file MyCamera.cpp
+@brief ƒJƒƒ‰
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+}
+//end basecross
---
+++ BaseCross/GameSources/MyCamera.h
@@ -0,0 +1,14 @@
+/*!
+@file MyCamera.h
+@brief ƒJƒƒ‰
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+	weak_ptr<GameObject> m_TargetObject;
+
+
+}
+//end basecross
