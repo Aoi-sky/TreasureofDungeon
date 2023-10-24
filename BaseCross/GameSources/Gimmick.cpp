41cb12878dc8b534add58559212e37f714493d67
yuu2611 <zeal22300025@fsgcl.jp>
2023-10-24 10:43:21 +0900

    ギミック用のヘッダーファイルとソースファイルを追加

---
+++ BaseCross/GameSources/Gimmick.cpp
@@ -0,0 +1,13 @@
+/*!
+@file Gimmick.cpp
+@brief —Î‚È‚Ç‚ÌÀ‘Ì
+*/
+
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross {
+
+
+}
+//end basecross
---
+++ BaseCross/GameSources/Gimmick.h
@@ -0,0 +1,13 @@
+/*!
+@file Gimmick.h
+@brief —Î‚È‚Ç‚ÌƒMƒ~ƒbƒN
+*/
+
+#pragma once
+#include "stdafx.h"
+
+namespace basecross {
+
+
+}
+//end basecross
