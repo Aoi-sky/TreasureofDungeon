d9db1172dfa05ff097031d30384239f86796f586
Aoi Oyake <zeal22300110@fsgcl.jp>
2023-12-15 14:15:33 +0900

    Player関連の更新

--- BaseCross/GameSources/Player.cpp
+++ BaseCross/GameSources/Player.cpp
@@ -20,6 +20,97 @@ namespace basecross{
 		);
 	}
 	
+	void Player::OnCreate() {
+		auto ptr = AddComponent<Transform>();
+		ptr->SetScale(0.5f, 0.5f, 0.5f);
+		ptr->SetRotation(0.0f, 0.0f, 0.0f);
+		ptr->SetPosition(Vec3(0.0f, 0.85f, 0.0f));
+
+		//衝突判定を付ける
+		auto ptrColl = AddComponent<CollisionCapsule>();
+		ptrColl->SetDrawActive(false);
+
+		//各パフォーマンスを得る
+		GetStage()->SetCollisionPerformanceActive(true);
+		GetStage()->SetUpdatePerformanceActive(true);
+		GetStage()->SetDrawPerformanceActive(true);
+
+		auto ptrGracity = AddComponent<Gravity>();
+
+		GetStage()->SetCollisionPerformanceActive(true);
+		GetStage()->SetUpdatePerformanceActive(true);
+		GetStage()->SetDrawPerformanceActive(true);
+
+		//影をつける（シャドウマップを描画する）
+		auto shadowPtr = AddComponent<Shadowmap>();
+		//影の形（メッシュ）を設定
+		shadowPtr->SetMultiMeshResource(L"M_PLAYER");
+
+		//描画コンポーネントの設定
+		auto  ptrDraw = AddComponent<BcPNTBoneModelDraw>();
+		// 新規ドローコンポーネントの設定
+		ptrDraw->SetMultiMeshResource(L"M_PLAYER");
+		ptrDraw->SetMeshToTransformMatrix(m_differenceMatrix);
+		//アニメーション設定
+		ptrDraw->AddAnimation(L"Wait", 10, 30, true);
+		ptrDraw->AddAnimation(L"WalkStart", 50, 10, false);
+		ptrDraw->AddAnimation(L"Walking1", 60, 10, false);
+		ptrDraw->AddAnimation(L"Walking2", 70, 10, false);
+		ptrDraw->AddAnimation(L"WalkEnd1", 80, 10, false);
+		ptrDraw->AddAnimation(L"WalkEnd2", 100, 10, false);
+		ptrDraw->AddAnimation(L"AttackStart", 120, 35, false);
+		ptrDraw->AddAnimation(L"AttackEnd", 155, 25, false);
+		ptrDraw->AddAnimation(L"Damage1", 190, 10, false);
+		ptrDraw->AddAnimation(L"Damage2", 210, 90, false);
+		ptrDraw->AddAnimation(L"Dead", 210, 30, false);
+		ptrDraw->ChangeCurrentAnimation(L"Wait");
+
+		//透明処理
+		SetAlphaActive(true);
+		SetDrawActive(true);
+
+		// タグの設定
+		AddTag(L"Player");
+
+		//カメラを取得
+		auto ptrCamera = dynamic_pointer_cast<MyCamera>(OnGetDrawCamera());
+		if (ptrCamera) {
+			//MyCameraである
+			//MyCameraに注目するオブジェクト（プレイヤー）の設定
+			ptrCamera->SetTargetObject(GetThis<GameObject>());
+			ptrCamera->SetTargetToAt(Vec3(0, 0.25f, 0));
+		}
+	}
+
+	void Player::OnUpdate() {
+		MovePlayer();
+		PlayerDead();
+		HitCheck();
+
+		auto& app = App::GetApp();
+		auto device = app->GetInputDevice(); // インプットデバイスオブジェクトを取得する
+		auto& pad = device.GetControlerVec()[0]; // １個目のコントローラーの状態を取得する
+		switch (m_motion)
+		{
+		case Wait:
+		case WalkStart:
+		case Walking1:
+		case Walking2:
+		case WalkEnd1:
+		case WalkEnd2:
+			// Xボタンが押されたら攻撃モーションに変更
+			if (pad.wPressedButtons & BUTTON_SHOT)
+			{
+				m_motion = AttackStart;
+			}
+			break;
+		default:
+			break;
+		}
+
+		AnimationUpdate();
+	}
+
 	Vec2 Player::GetInputState() const{
 		Vec2 ret;
 		auto controllerVec = App::GetApp()->GetInputDevice().GetControlerVec();//コントローラ取得
@@ -63,118 +154,216 @@ namespace basecross{
 	void Player::MovePlayer() {
 		float elapsedTime = App::GetApp()->GetElapsedTime();
 		auto angle = GetMoveVector();
-		if (angle.length() > 0.0f) {
-			auto pos = GetComponent<Transform>()->GetPosition();
-			pos += angle * elapsedTime * m_Speed;
-			GetComponent<Transform>()->SetPosition(pos);
+		if (angle.length() > 0.0f && m_motion == Wait) {
+			m_motion = WalkStart;
 		}
-		//回転の計算
-		if (angle.length() > 0.0f) {
-			auto utilPtr = GetBehavior<UtilBehavior>();
-			utilPtr->RotToHead(angle, 1.0f);
+		switch (m_motion)
+		{
+		case Wait:
+		case WalkStart:
+		case Walking1:
+		case Walking2:
+		case WalkEnd1:
+		case WalkEnd2:
+			if (angle.length() > 0.0f) {
+				auto pos = GetComponent<Transform>()->GetPosition();
+				pos += angle * elapsedTime * m_Speed;
+				GetComponent<Transform>()->SetPosition(pos);
+			}
+			//回転の計算
+			if (angle.length() > 0.0f) {
+				auto utilPtr = GetBehavior<UtilBehavior>();
+				utilPtr->RotToHead(angle, 1.0f);
+			}
+			break;
+		case AttackStart:
+			//回転の計算
+			if (angle.length() > 0.0f) {
+				auto utilPtr = GetBehavior<UtilBehavior>();
+				utilPtr->RotToHead(angle, 1.0f);
+			}
+			break;
+		default:
+			break;
+		}		
+	}
+
+	void Player::OnAttack() {
+		// プレイヤーが所属している「ステージ」を取得し、
+		// そこにBulletオブジェクトを追加する
+		GetStage()->AddGameObject<Wave>(GetThis<Player>()); //自分自身のオブジェクトのポインタを取得する
+	}
+
+	void Player::HitCheck() {
+		totalTime += totalTime < invincibleTime;
+		if (HitFlg && totalTime >= invincibleTime) {
+			AddPlayerDamage(10, Damage1);
+			totalTime = 0;
 		}
 	}
 
 	//ダメージ関数
-	void Player::AddPlayerDamage(int damage) {
+	void Player::AddPlayerDamage(int damage, eMotion Motion) {
+		switch (m_motion)
+		{
+		case Wait:
+		case WalkStart:
+		case Walking1:
+		case Walking2:
+		case WalkEnd1:
+		case WalkEnd2:
+			m_motion = Motion;
+			break;
+		case AttackStart:
+		case AttackEnd:
+			if (Motion == Damage2) {
+				m_motion = Motion;
+			}
+			break;
+
+		default:
+			break;
+		}
+
 		m_Life -= damage;
+		if (m_Life < 0) {
+			m_Life = 0;
+		}
+
 	}
 
 	void Player::PlayerDead() {
 		if (m_Life <= 0) {
-			SetDrawActive(false);
-			PostEvent(1.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameOverStage");
-		}
-	}
-	
-	void Player::OnCreate() {
-		auto ptr = AddComponent<Transform>();
-		ptr->SetScale(0.5f, 0.5f, 0.5f);
-		ptr->SetRotation(0.0f, 0.0f, 0.0f);
-		ptr->SetPosition(Vec3(0.0f, 0.85f, 0.0f));
-
-		//衝突判定を付ける
-		auto ptrColl = AddComponent<CollisionCapsule>();
-		ptrColl->SetDrawActive(false);
-
-		//各パフォーマンスを得る
-		GetStage()->SetCollisionPerformanceActive(true);
-		GetStage()->SetUpdatePerformanceActive(true);
-		GetStage()->SetDrawPerformanceActive(true);
-
-		auto ptrGracity = AddComponent<Gravity>();
-
-		GetStage()->SetCollisionPerformanceActive(true);
-		GetStage()->SetUpdatePerformanceActive(true);
-		GetStage()->SetDrawPerformanceActive(true);
-
-		//影をつける（シャドウマップを描画する）
-		auto shadowPtr = AddComponent<Shadowmap>();
-		//影の形（メッシュ）を設定
-		shadowPtr->SetMultiMeshResource(L"M_PLAYER");
-
-		//描画コンポーネントの設定
-		auto  ptrDraw = AddComponent<BcPNTBoneModelDraw>();
-		// 新規ドローコンポーネントの設定
-		ptrDraw->SetMultiMeshResource(L"M_PLAYER");
-		ptrDraw->SetMeshToTransformMatrix(m_differenceMatrix);
-		//アニメーション設定
-		ptrDraw->AddAnimation(L"Wait", 0, 1, true, 10);
-		ptrDraw->AddAnimation(L"Walk", 5,40 ,false, 10);
-		ptrDraw->AddAnimation(L"Attack", 50, 60, false,10);
-		ptrDraw->ChangeCurrentAnimation(L"Walk");
-
-		//透明処理
-		SetAlphaActive(true);
-		SetDrawActive(true);
-
-		// タグの設定
-		AddTag(L"Player");
-
-		//カメラを取得
-		auto ptrCamera = dynamic_pointer_cast<MyCamera>(OnGetDrawCamera());
-		if (ptrCamera) {
-			//MyCameraである
-			//MyCameraに注目するオブジェクト（プレイヤー）の設定
-			ptrCamera->SetTargetObject(GetThis<GameObject>());
-			ptrCamera->SetTargetToAt(Vec3(0, 0.25f, 0));
+			m_motion = Dead;
 		}
 	}
 
-	void Player::OnUpdate() {
-		MovePlayer();
-		PlayerDead();
-		auto& app = App::GetApp();
-		float delta = app->GetElapsedTime(); // 前フレームからの経過時間（60FPS）
-
-		float elapsedTime = App::GetApp()->GetElapsedTime();
-		//アニメーションを更新する
+	void Player::AnimationUpdate()
+	{
+		// アニメーションの再生
 		auto ptrDraw = GetComponent<BcPNTBoneModelDraw>();
-		ptrDraw->UpdateAnimation(elapsedTime);		
-
-
-		auto device = app->GetInputDevice(); // インプットデバイスオブジェクトを取得する
-		auto& pad = device.GetControlerVec()[0]; // １個目のコントローラーの状態を取得する
-		// Xボタンが押されたら弾を発射する
-		if (pad.wPressedButtons & BUTTON_SHOT)
+		// アニメーションのタイプが変わっていたら
+		if (m_motion != m_currentMotion || ptrDraw->GetCurrentAnimation() != m_motionKey.at(m_motion))
 		{
-			ptrDraw->ChangeCurrentAnimation(L"Attack");
-			AddPlayerDamage(5);
-			// プレイヤーが所属している「ステージ」を取得し、
-			// そこにBulletオブジェクトを追加する
-			GetStage()->AddGameObject<Wave>(GetThis<Player>()); //自分自身のオブジェクトのポインタを取得する
+			// タイプに応じてアニメーションを変更する
+			ptrDraw->ChangeCurrentAnimation(m_motionKey.at(m_motion));
+			m_currentMotion = m_motion;
 		}
+
+		// 前フレームからのデルタタイムを取得
+		float deltaTime = App::GetApp()->GetElapsedTime();
+
+		// モーションに応じて再生する
+		switch (m_motion)
+		{
+		case Wait:
+			ptrDraw->UpdateAnimation(deltaTime * 1.0f);
+			break;
+		case WalkStart:
+			ptrDraw->UpdateAnimation(deltaTime * 2.0f);
+			break;
+		case Walking1:
+			ptrDraw->UpdateAnimation(deltaTime * 1.0f);
+			break;
+		case Walking2:
+			ptrDraw->UpdateAnimation(deltaTime * 1.0f);
+			break;
+		case WalkEnd1:
+			ptrDraw->UpdateAnimation(deltaTime * 2.0f);
+			break;
+		case WalkEnd2:
+			ptrDraw->UpdateAnimation(deltaTime * 2.0f);
+			break;
+		case AttackStart:
+			ptrDraw->UpdateAnimation(deltaTime * 2.0f);
+			break;
+		case AttackEnd:
+			ptrDraw->UpdateAnimation(deltaTime * 2.0f);
+			break;
+		case Damage1:
+			ptrDraw->UpdateAnimation(deltaTime * 1.0f);
+			break;
+		case Damage2:
+			ptrDraw->UpdateAnimation(deltaTime * 1.0f);
+			break;
+		case Dead:
+			ptrDraw->UpdateAnimation(deltaTime * 0.5f);
+			break;
+		default:
+			ptrDraw->UpdateAnimation(deltaTime * 1.0f);
+			break;
+		}
+
+		// アニメーションが終了しているかをチェック
+		if (ptrDraw->IsTargetAnimeEnd()) {
+			auto angle = GetMoveVector();
+
+			switch (m_currentMotion)
+			{
+			case Wait:
+				break;
+			case WalkStart:
+				if (angle.length() > 0.0f) {
+					m_motion = Walking1;
+				}
+				else {
+					m_motion = WalkEnd1;
+				}
+				break;
+			case Walking1:
+				if (angle.length() > 0.0f) {
+					m_motion = Walking2;
+				}
+				else {
+					m_motion = WalkEnd2;
+				}
+				break;
+			case Walking2:
+				if (angle.length() > 0.0f) {
+					m_motion = Walking1;
+				}
+				else {
+					m_motion = WalkEnd1;
+				}
+				break;
+			case AttackStart:
+				OnAttack();
+				m_motion = AttackEnd;
+				break;
+			case AttackEnd:
+			case Damage1:
+			case Damage2:
+				if (angle.length() > 0.0f) {
+					m_motion = WalkStart;
+				}
+				else {
+					m_motion = Wait;
+				}
+				break;
+			case Dead:
+				SetDrawActive(false);
+				PostEvent(1.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameOverStage");
+				break;
+			default:
+				m_motion = Wait;
+				break;
+			}
+		}
+
 	}
 
 	void Player::OnCollisionEnter(shared_ptr<GameObject>& Other) {
 		if (Other->FindTag(L"Golem"))
 		{
-			AddPlayerDamage(5);
+			HitFlg = true;
 		}
 	}
 
 	void Player::OnCollisionExit(shared_ptr<GameObject>& Other) {
-		
+		if (Other->FindTag(L"Golem"))
+		{
+			HitFlg = false;
+		}
 	}
 
 }
